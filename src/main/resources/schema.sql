36442142a03ad8f937894598938067756f1b098b
jalexisrdv <jalexisrdv@outlook.com>
2025-11-02 15:33:38 -0600

    chore: database schema
    - Add tables for inventory

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,3 +1,7 @@
+DROP TABLE IF EXISTS inventory_out_movements CASCADE;
+DROP TABLE IF EXISTS inventory_entry_movements CASCADE;
+DROP TABLE IF EXISTS inventory CASCADE;
+DROP TABLE IF EXISTS item_category CASCADE;
 DROP TABLE IF EXISTS user_roles CASCADE;
 DROP TABLE IF EXISTS role_permissions CASCADE;
 DROP TABLE IF EXISTS permissions CASCADE;
@@ -41,7 +45,63 @@ CREATE TABLE user_roles (
     id SERIAL PRIMARY KEY,
     user_id INT NOT NULL,
     role_id INT NOT NULL,
-    CONSTRAINT fk_role_members_users FOREIGN KEY (user_id) REFERENCES users(id),
-    CONSTRAINT fk_role_members_roles FOREIGN KEY (role_id) REFERENCES roles(id),
-    CONSTRAINT uq_role_members_user_role UNIQUE (user_id, role_id)
+    CONSTRAINT fk_user_roles_users FOREIGN KEY (user_id) REFERENCES users(id),
+    CONSTRAINT fk_user_roles_roles FOREIGN KEY (role_id) REFERENCES roles(id),
+    CONSTRAINT uq_user_roles_user_role UNIQUE (user_id, role_id)
+);
+
+CREATE TABLE item_category (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(500)
+);
+
+CREATE TABLE inventory (
+    id SERIAL PRIMARY KEY,
+    item_category_id VARCHAR(100) NOT NULL,
+    item_code VARCHAR(50) UNIQUE NOT NULL,
+    item_name TEXT,
+    minimum_stock NUMERIC(10,2) DEFAULT 0,
+    created_by INT,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_by INT,
+    updated_at TIMESTAMP,
+    CONSTRAINT fk_inventory_users_created_by FOREIGN KEY (created_by) REFERENCES users(id),
+    CONSTRAINT fk_inventory_users_updated_by FOREIGN KEY (updated_by) REFERENCES users(id)
+);
+
+CREATE TABLE inventory_entry_movements (
+    id SERIAL PRIMARY KEY,
+    inventory_id INT NOT NULL,
+    quantity NUMERIC(10,2) NOT NULL CHECK (quantity > 0),
+    created_by INT,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_by INT,
+    updated_at TIMESTAMP,
+    reviewed_by INT,
+    reviewed_at TIMESTAMP,
+    status VARCHAR(20) DEFAULT 'PENDIENTE' CHECK (status IN ('PENDIENTE', 'APROBADO', 'RECHAZADO')),
+    rejected_reason TEXT,
+    CONSTRAINT fk_inventory_entry_movements_inventory FOREIGN KEY (inventory_id) REFERENCES inventory(id),
+    CONSTRAINT fk_inventory_entry_movements_users_created_by FOREIGN KEY (created_by) REFERENCES users(id),
+    CONSTRAINT fk_inventory_entry_movements_users_updated_by FOREIGN KEY (updated_by) REFERENCES users(id),
+    CONSTRAINT fk_inventory_entry_movements_users_reviewed_by FOREIGN KEY (reviewed_by) REFERENCES users(id)
+);
+
+CREATE TABLE inventory_output_movements (
+    id SERIAL PRIMARY KEY,
+    inventory_id INT NOT NULL,
+    quantity NUMERIC(10,2) NOT NULL CHECK (quantity > 0),
+    output_reason TEXT,
+    created_by INT,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_by INT,
+    updated_at TIMESTAMP,
+    reviewed_by INT,
+    reviewed_at TIMESTAMP,
+    status VARCHAR(20) DEFAULT 'PENDIENTE' CHECK (status IN ('PENDIENTE', 'APROBADO', 'RECHAZADO')),
+    rejected_reason TEXT,
+    CONSTRAINT fk_inventory_output_movements_inventory FOREIGN KEY (inventory_id) REFERENCES inventory(id),
+    CONSTRAINT fk_inventory_output_movements_users_created_by FOREIGN KEY (created_by) REFERENCES users(id),
+    CONSTRAINT fk_inventory_output_movements_users_updated_by FOREIGN KEY (updated_by) REFERENCES users(id),
+    CONSTRAINT fk_inventory_output_movements_users_reviewed_by FOREIGN KEY (reviewed_by) REFERENCES users(id)
 );
